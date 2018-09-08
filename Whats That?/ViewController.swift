e2e012ea97e8216e58aef308cad06d1556c18ec5
James Buckley <j@mesbuckley.com>
2018-09-09 00:08:56 +0100

    Initial Commit

---
+++ Whats That?/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Whats That?
+//
+//  Created by James Buckley on 09/09/2018.
+//  Copyright © 2018 James Buckley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
